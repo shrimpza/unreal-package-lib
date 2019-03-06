e59d3d0992f90977d877d647957cdf53a37118fd
Kenneth Watson <git@shrimpworks.za.net>
2019-03-06 20:10:24 +0200

    export more packages

--- src/module-info.java
+++ src/module-info.java
@@ -2,4 +2,7 @@ module unreal.packages.lib {
 	requires java.desktop;
 
 	exports net.shrimpworks.unreal.packages;
+	exports net.shrimpworks.unreal.packages.entities;
+	exports net.shrimpworks.unreal.packages.entities.objects;
+	exports net.shrimpworks.unreal.packages.entities.properties;
 }
