b8825ae004af1ec5afb2a75ab4ade32cc4c20385
Kenneth Watson <git@shrimpworks.za.net>
2020-07-25 20:32:34 +0200

    better ability to test UT3 package

--- src/test/java/net/shrimpworks/unreal/packages/PackageTest.java
+++ src/test/java/net/shrimpworks/unreal/packages/PackageTest.java
@@ -3,11 +3,18 @@ package net.shrimpworks.unreal.packages;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.Objects;
 import java.util.zip.GZIPInputStream;
 import javax.imageio.ImageIO;
 
@@ -26,18 +33,20 @@ import net.shrimpworks.unreal.packages.entities.properties.StringProperty;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PackageTest {
 
+	private static final String UT3_MAP_URL = "https://code.shrimpworks.za.net/artefacts/net/shrimpworks/unreal-package-lib/resources/DM-MCC-Morbias.ut3.gz";
+
 	private static Path unrMap;
 	private static Path ut2Map;
+	private static Path ut3Map;
 
 	@BeforeAll
-	public static void setup() throws IOException {
+	public static void setup() throws IOException, InterruptedException {
 		// unpack our test map to a temporary location
 		unrMap = Files.createTempFile("test-map-", ".unr");
 		try (InputStream is = PackageTest.class.getResourceAsStream("SCR-CityStreet.unr.gz");
@@ -49,6 +58,22 @@ public class PackageTest {
 		try (InputStream is = PackageTest.class.getResourceAsStream("UWRM-Torpedo.ut2.gz");
 			 GZIPInputStream gis = new GZIPInputStream(is)) {
 			Files.copy(gis, ut2Map, StandardCopyOption.REPLACE_EXISTING);
+		}
+
+		ut3Map = fetchAndCache(UT3_MAP_URL, Paths.get(System.getProperty("java.io.tmpdir", "/tmp")).resolve("DM-MMC-Morbias.ut3"));
+	}
+
+	private static Path fetchAndCache(String url, Path output) throws IOException, InterruptedException {
+		if (Files.exists(output)) return output;
+
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest req = HttpRequest.newBuilder(URI.create(url)).GET().build();
+		System.out.printf("Downloading %s to %s%n", url, output.toAbsolutePath());
+		HttpResponse<InputStream> send = client.send(req, HttpResponse.BodyHandlers.ofInputStream());
+		try (InputStream is = send.body();
+			 GZIPInputStream gis = new GZIPInputStream(is)) {
+			Files.copy(gis, output, StandardCopyOption.REPLACE_EXISTING);
+			return output;
 		}
 	}
 
@@ -84,25 +109,28 @@ public class PackageTest {
 	}
 
 	@Test
-	@Disabled
 	public void meh() throws IOException {
-		try (PackageReader reader = new PackageReader(Paths.get("/home/shrimp/tmp/DM-MCC-Morbias.ut3"), false);
+//		try (PackageReader reader = new PackageReader(Paths.get("/home/shrimp/tmp/DM-MCC-Morbias-UPK.ut3"), false);
+		try (PackageReader reader = new PackageReader(ut3Map, false);
 			 Package pkg = new Package(reader)) {
 			assertNotNull(pkg.sha1Hash());
 
-//			Arrays.stream(pkg.objects)
-//				  .filter(Objects::nonNull)
-//				  .sorted(Comparator.comparing(e -> e.name.name))
-//				  .forEach(e -> {
-//					  try {
-////						  System.out.printf("%s :: %s%n", e.classIndex.get().name().name, e.name.name);
-//						  for (Property property : e.object().properties) {
-////							  System.out.printf("  - %s :: %s%n", property.getClass().getSimpleName(), property.name.name);
-//						  }
-//					  } catch (Throwable ex) {
-//						  // skip
-//					  }
-//				  });
+			Arrays.stream(pkg.objects)
+				  .filter(Objects::nonNull)
+				  .sorted(Comparator.comparing(e -> e.name.name))
+				  .forEach(e -> {
+					  try {
+						  System.out.printf("%s :: %s%n", e.classIndex.get().name().name, e.name.name);
+						  for (Property property : e.object().properties) {
+							  System.out.printf("  - %s :: %s%n", property.getClass().getSimpleName(), property.name.name);
+						  }
+						  System.out.println("----");
+					  } catch (Throwable ex) {
+						  ex.printStackTrace();
+						  // bomb out
+						  fail(ex);
+					  }
+				  });
 
 //			ExportedObject o = pkg.objectByName(new Name("Texture2D"));
 //			pkg.objectsByClassName("Texture2D").forEach(o -> {
@@ -112,26 +140,26 @@ public class PackageTest {
 //				}
 //			});
 
-			ExportedObject o = pkg.objectByName(new Name("MorbiasScreenshot"));
-			System.out.printf("%s :: %s%n", o.classIndex.get().name().name, o.name.name);
-			Object obj = o.object();
-			for (Property property : obj.properties) {
-				System.out.println(property.getClass().getSimpleName() + ": " + property.toString());
-			}
-			assertTrue(obj instanceof Texture);
-
-			Path tmpScreenshot = Files.createTempFile("test-ss-", ".png");
-
-			try {
-				Texture.MipMap[] mipMaps = ((Texture)obj).mipMaps();
-				BufferedImage bufferedImage = mipMaps[0].get();
-				ImageIO.write(bufferedImage, "png", tmpScreenshot.toFile());
-
-				// attempt to load the saved screenshot, should work if it was written correctly
-				assertNotNull(ImageIO.read(tmpScreenshot.toFile()));
-			} finally {
-				Files.deleteIfExists(tmpScreenshot);
-			}
+//			ExportedObject o = pkg.objectByName(new Name("MorbiasScreenshot"));
+//			System.out.printf("%s :: %s%n", o.classIndex.get().name().name, o.name.name);
+//			Object obj = o.object();
+//			for (Property property : obj.properties) {
+//				System.out.println(property.getClass().getSimpleName() + ": " + property.toString());
+//			}
+//			assertTrue(obj instanceof Texture);
+//
+//			Path tmpScreenshot = Files.createTempFile("test-ss-", ".png");
+//
+//			try {
+//				Texture.MipMap[] mipMaps = ((Texture)obj).mipMaps();
+//				BufferedImage bufferedImage = mipMaps[0].get();
+//				ImageIO.write(bufferedImage, "png", tmpScreenshot.toFile());
+//
+//				// attempt to load the saved screenshot, should work if it was written correctly
+//				assertNotNull(ImageIO.read(tmpScreenshot.toFile()));
+//			} finally {
+//				Files.deleteIfExists(tmpScreenshot);
+//			}
 
 			System.out.println(reader.stats);
 		}
